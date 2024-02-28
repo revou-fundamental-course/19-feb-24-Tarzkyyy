d91bf2287d741eefd021d7bcdb57183b90e61d12
Tarzkyyy <160499374+Tarzkyyy@users.noreply.github.com>
2024-02-28 08:39:29 +0700

    Update Script.js

---
+++ JS/Script.js
@@ -0,0 +1,25 @@
+const nama = "Sepatu Vans";
+let Warna = Greenies;
+
+let Jenis = document.getElementById('Jenis');
+console.log('Jenis');
+
+function generateJenis() {
+    let Jenis;
+
+    if(Nama "Greenies") {
+     Jenis = "Greenies";
+
+    } else if (Nama "Skyies") {
+     Jenis = "Skyies";
+    }
+    else if(Nama "Pinkies"){
+     Jenis ="Pinkies";
+
+   return Jenis.innerHTML = generasi;
+}
+
+console.log(nama);
+console.log(Warna);
+
+generateJenis()
